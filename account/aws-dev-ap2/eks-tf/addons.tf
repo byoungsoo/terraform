2f520b3ecbd75bfd631334ea5e6a12c0b412f0dd
byoungsoo <skwltg90@naver.com>
2026-04-03 21:42:20 +0900

    refactor: move pod_identity_association into aws_eks_addon dynamic block

--- account/aws-dev-ap2/eks-tf/addons.tf
+++ account/aws-dev-ap2/eks-tf/addons.tf
@@ -10,19 +10,13 @@ resource "aws_eks_addon" "this" {
   resolve_conflicts_on_update = each.value.resolve_conflicts_on_update
   configuration_values        = each.value.configuration_values
 
-  tags = var.common_tags
-}
-
-################################################################################
-# Pod Identity Associations (conditional)
-################################################################################
-resource "aws_eks_pod_identity_association" "this" {
-  for_each = {
-    for k, v in var.eks_addons : k => v if v.pod_identity_role_name != null
+  dynamic "pod_identity_association" {
+    for_each = each.value.pod_identity_role_name != null ? [1] : []
+    content {
+      role_arn         = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${each.value.pod_identity_role_name}"
+      service_account  = each.value.pod_identity_service_account
+    }
   }
 
-  cluster_name    = aws_eks_cluster.main.name
-  namespace       = each.value.pod_identity_namespace
-  service_account = each.value.pod_identity_service_account
-  role_arn        = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${each.value.pod_identity_role_name}"
+  tags = var.common_tags
 }
